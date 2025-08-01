90ae39a027ef2214b762ff205db6ea2d0a73b50b
Ali El Boussi <alielbousi@gmail.com>
2025-08-01 05:16:26 +0200

    Update project with latest changes

---
+++ ClosingStock/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.webkit.WebChromeClient
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    private val allowedUrl = "https://bestrest-delta.vercel.app/closing-stock"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Disable the back button using OnBackPressedDispatcher
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Do nothing
+            }
+        })
+
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.webChromeClient = WebChromeClient()
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                val url = request?.url.toString()
+                // Only allow the closing stock page
+                return url != allowedUrl
+            }
+        }
+
+        webView.loadUrl(allowedUrl)
+    }
+}
